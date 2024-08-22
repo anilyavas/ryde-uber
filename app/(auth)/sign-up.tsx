f7259b8f42212b3baa4129a26b470fde6864fbbf
anilyavas <anil.yavas99@gmail.com>
2024-08-22 14:14:11 +0300

    custom inputfield comp

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,12 +1,33 @@
-import { View, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import InputField from '@/components/InputField';
+import { icons, images } from '@/constants';
+import { useState } from 'react';
+import { View, Text, ScrollView, Image } from 'react-native';
 
 export default function Signup() {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
   return (
-    <SafeAreaView className='flex-1'>
-      <View className='flex-1 items-center justify-center'>
-        <Text>Hello</Text>
+    <ScrollView className='flex-1 bg-white'>
+      <View className='flex-1 bg-white'>
+        <View className='relative w-full h-[250px]'>
+          <Image className='z-0 w-full h-[250px]' source={images.signUpCar} />
+          <Text className='text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5'>
+            Create Your Account
+          </Text>
+        </View>
+        <View className='p-5'>
+          <InputField
+            label='Name'
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setForm({ ...form, name: value })}
+          />
+        </View>
       </View>
-    </SafeAreaView>
+    </ScrollView>
   );
 }
---
+++ components/InputField.tsx
@@ -0,0 +1,48 @@
+import {
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  View,
+  Text,
+  Image,
+  TextInput,
+  Platform,
+  Keyboard,
+} from 'react-native';
+
+export default function InputField({
+  label,
+  labelStyle,
+  icon,
+  secureTextEntry = false,
+  containerStyle,
+  inputStyle,
+  iconStyle,
+  className,
+  ...props
+}) {
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View className='w-full my-2'>
+          <Text className={`text-lg font-JakartaSemiBold mb-3 ${labelStyle}`}>
+            {label}
+          </Text>
+          <View
+            className={`flex flex-row justify-start items-center rounded-full border border-neutral-100 focus:border-primary-500 bg-neutral-100 relative ${containerStyle}`}
+          >
+            {icon && (
+              <Image source={icon} className={`w-6 h-6 ml-4 ${iconStyle}`} />
+            )}
+            <TextInput
+              className={`rounded-full p-4 font-JakartaSemiBold text-[15px] flex-1 ${inputStyle} text-left`}
+              secureTextEntry={secureTextEntry}
+              {...props}
+            />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+}
